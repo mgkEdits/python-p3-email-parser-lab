ac2a47af8f3b2acede6b069b8f4a482b2adf265c
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-12-05 12:43:34 +0300

    cmplt lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,23 @@
 # your code goes here!
+
+import re
+
+class EmailAddressParser:
+    regex_mail = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Use regular expression to find email addresses in the input string
+        address_list = re.split(r'[,\s]+', self.email_addresses)
+
+        mail_addresses = set()
+        for address in address_list:
+            if self.regex_mail.fullmatch(address):
+                mail_addresses.add(address)
+
+        # Remove duplicates and sort alphabetically
+        unique_addresses = sorted(list(mail_addresses))
+
+        return unique_addresses
